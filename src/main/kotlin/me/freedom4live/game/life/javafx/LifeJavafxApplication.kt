446e90c56b0c4c33e4f68cf1fb7615cb5afefdb7
Aleksei Novikov <aleksei.novikov@betpawa.com>
2019-09-26 15:37:34 +0300

    Initial commit

---
+++ src/main/kotlin/me/freedom4live/game/life/javafx/LifeJavafxApplication.kt
@@ -0,0 +1,11 @@
+package me.freedom4live.game.life.javafx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LifeJavafxApplication
+
+fun main(args: Array<String>) {
+    runApplication<LifeJavafxApplication>(*args)
+}
---
+++ src/test/kotlin/me/freedom4live/game/life/javafx/LifeJavafxApplicationTests.kt
@@ -0,0 +1,16 @@
+package me.freedom4live.game.life.javafx
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class LifeJavafxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
