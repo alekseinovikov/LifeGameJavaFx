f0feb31cbd942c2a6cd85ca25ce337ba63a9e65e
Aleksei Novikov <aleksei.novikov@betpawa.com>
2019-09-26 22:14:03 +0300

    Configured JavaFx project and created main stage with main scene to run with configurations

--- src/main/kotlin/me/freedom4live/game/life/javafx/application/creator/FxEngine.kt
+++ src/main/kotlin/me/freedom4live/game/life/javafx/application/creator/FxEngine.kt
@@ -1,22 +1,23 @@
 package me.freedom4live.game.life.javafx.application.creator
 
+import javafx.scene.Group
 import javafx.scene.Scene
-import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 import me.freedom4live.game.life.javafx.configuration.ConfigurationProvider
 import org.springframework.stereotype.Component
+import kotlin.math.roundToInt
 
 @Component
 class FxEngine(private val configurationProvider: ConfigurationProvider) : Engine {
 
-    override fun start(mainStage: Stage) {
-        val rootScene = createAndConfigureRootScene()
-        configureAndRunMainStage(mainStage, rootScene)
+    override fun start(stage: Stage) {
+        val group = Group()
+        val rootScene = createAndConfigureRootScene(group)
+        configureAndRunMainStage(stage, rootScene)
     }
 
-    private fun createAndConfigureRootScene(): Scene {
-        val rootPane = StackPane()
-        return Scene(rootPane, windowWeight, windowHeight)
+    private fun createAndConfigureRootScene(group: Group): Scene {
+        return Scene(group, windowWeight, windowHeight)
     }
 
     private fun configureAndRunMainStage(stage: Stage, scene: Scene) {
@@ -26,6 +27,27 @@ class FxEngine(private val configurationProvider: ConfigurationProvider) : Engin
         stage.show()
     }
 
+    private fun createSquares(group: Group) {
+        var x = 0
+        var y = 0
+
+        for (w in 0..weightSquareCount) {
+            for (h in 0..heightSquareCount) {
+                group.
+
+                y += squareHeight.toInt()
+            }
+
+            x += squareWeight.toInt()
+        }
+    }
+
+    private val heightSquareCount: Int
+        get() = (windowHeight / squareHeight).roundToInt()
+
+    private val weightSquareCount: Int
+        get() = (windowWeight / squareWeight).roundToInt()
+
     private val windowTitle: String
         get() = configurationProvider.windowTitle()
 
@@ -34,5 +56,11 @@ class FxEngine(private val configurationProvider: ConfigurationProvider) : Engin
 
     private val windowWeight: Double
         get() = configurationProvider.windowWeight()
+
+    private val squareHeight: Double
+        get() = configurationProvider.squareHeight()
+
+    private val squareWeight: Double
+        get() = configurationProvider.squareWeight()
 
 }
